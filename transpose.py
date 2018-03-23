a04ccdc511e761f50c1f254486f7a1d5762c74bb
Vivian <vtan@utexas.edu>
2018-03-23 14:50:20 -0500

    make velodyne capture faster

--- transpose.py
+++ transpose.py
@@ -17,17 +17,20 @@ inputs:
 
 #R = np.linalg.inv(R)
 #replace the name of this later
+# GET LIDAR DATA HERE:
 df = pd.read_csv("truck.csv")
 print(df.shape)
 
 df = df[df['Points_m_XYZ:0']>=0]
 df = df[df['distance_m']<=30]
 
+# get x, y, z, and distance data from dataframe 
 X= df['Points_m_XYZ:0']
 Y= df['Points_m_XYZ:1']
 Z= df['Points_m_XYZ:2']
 distance = df['distance_m']
 
+# GET BOUNDING BOX DATA HERE: (hard coded for now)
 x1 = 50
 x2 = 500
 y1 = 100
@@ -40,18 +43,23 @@ zr = 1.58
 
 # start z by 90 y by -90
 
+# Get R matrix
 Xr = np.matrix([[1,0,0],[0,math.cos(xr),-1*math.sin(xr)],[0,math.sin(xr),math.cos(xr)]])
 Yr = np.matrix([[math.cos(yr),0,math.sin(yr)],[0,1,0],[-1*math.sin(yr),0,math.cos(yr)]])
 Zr = np.matrix([[math.cos(zr),-1*math.sin(zr),0],[math.sin(zr),math.cos(zr),0],[0,0,1]])
 R = np.matmul(Zr,Yr,Xr)
 
+# transpose matrix?
 T = np.matrix([[-1],[3],[.7]])
+# get image
 img = plt.imread('single_truck.jpg')
+# plot image
 fig,ax = plt.subplots(1)
 plt.xlim(0, 720)
 plt.ylim(450,0)
 ax.imshow(np.fliplr(img))
 
+# make A matrix (x y z)
 X1= X.values
 size= len(X1)
 X1= np.matrix.transpose(X1)
@@ -62,20 +70,19 @@ Z1= np.matrix.transpose(Z1)
 A=[X1,Y1,Z1]
 A= np.matrix([X1,Y1 ,Z1])
 
+# multiply matrices
 T1=np.matrix.transpose(T)
 T2= np.repeat(T1,size,axis=0)
-
 T2= np.matrix.transpose(T2)
-
 now= time.time()
 c2 = 100*np.matmul((R),(A-T2))
 
-
+# plot points?
 for i in range(size):    
    circ = Circle((c2[0,i], c2[1,i]), .5, color='blue' )
    ax.add_patch(circ)
 
-
+# ???
 xcenter= (x1+x2)/2.0
 ycenter= (y1+y2)/2.0
 
@@ -85,6 +92,8 @@ B= np.square((c3[0,:]-xcenter))+ np.square((c3[1,:]-ycenter))
 
 index0= np.argmin(B, axis=1)
 print(index0)
+
+# check time elapsed
 runtime= time.time()- now
 print("Matrix calculation runtime:" ,runtime)
 
--- velodyne_capture_v2.py
+++ velodyne_capture_v2.py
@@ -14,17 +14,29 @@ soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 soc.bind(('', PORT))
 
 vertical_angle = [-15, 1, -13, -3, -11, 5, -9, 7, -7, 9, -5, 11, -3, 13, -1, 15]
-count = 0;
+count = 0
+count1 = 0
 initial_angle = 0
-data_buff = pd.DataFrame(columns=['x', 'y', 'z', 'distance'])
+#data_buff = pd.DataFrame(columns=['x', 'y', 'z', 'distance'])
+data_buff = []
+
 prev_time = datetime.now()
 
 while True:
     # get data from port
     data = soc.recv(1248)
+    print(len(data))
 
-    # get all data except the last 2 bytes? 
+    # get all data except the last 2 bytes
     raw_data = data[:-2]
+    count += 1
+    if(count==90):
+        print(data_buff)
+        #data_buff =  pd.DataFrame(columns=['x', 'y', 'z', 'distance'])
+        data_buff = []
+        #reset counter
+        count = 0
+
 
     # for each of the 12 data blocks: 
     for k in range(12):
@@ -39,38 +51,7 @@ while True:
         '''
         azimuth = raw_data[2+offset] | (raw_data[3+offset] << 8)
         azimuth = azimuth / 100
-
-        # # see how long it takes to send one sweep
-        # if(int(azimuth) == initial_angle):
-        #     timestamp = int.from_bytes(raw_data[-4:], byteorder='little')
-        #     timestamp = timestamp/1000 # convert from microseconds, gives milliseconds
-        #     if(count%64==0):
-        #         print('time diff: %f, angle: %f' % (timestamp-prev_time, azimuth))
-        #         prev_time = timestamp
-        #         flag = 1
-        #     count += 1
-        
-        # check time using packet info (not used)
-        # timestamp = int.from_bytes(raw_data[-4:], byteorder='little')
-        # timestamp = timestamp/1000 # convert from microseconds, gives milliseconds
-        # current_time= timestamp
-        current_time = datetime.now()
-        
-        if(((current_time-prev_time).microseconds/1000)>100):
-            # print data in buffer
-            print(data_buff)
-
-            # check time elapsed:
-            # print((current_time-prev_time).microseconds/1000)
-            # check length of data buffer:
-            # print(len(data_buff))
-
-            # reset time
-            prev_time = current_time
-            # clear buffer
-            data_buff =  pd.DataFrame(columns=['x', 'y', 'z', 'distance'])
-            # reset counter
-            count = 0
+        # print(azimuth)
 
         for i in range(2):
             for j in range(16):
@@ -85,7 +66,11 @@ while True:
                 z = distance*math.sin(azimuth2)
 
                 # limit fov 
-                if(45 <= azimuth <= 135):
+                #if(45 <= azimuth <= 135):
                     # print('angle1: %f\tangle2: %f\t x: %f\ty: %f\tz: %f\tdistance: %f\t' % (azimuth, azimuth2, x, y, z, distance))
-                    data_buff.loc[count] = [x, y, z, distance]
-                    count += 1
+               # data_buff.loc[count1] = [x, y, z, distance]
+                data_buff.append([x,y,z,distance])
+                count1+=1
+                
+
+
